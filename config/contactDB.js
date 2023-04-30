d81d49840da678092aa87a3f0bb11351548f4367
SiskoWeb <sisko4dev@gmail.com>
2023-04-30 12:43:07 +0100

    first commit

--- config/contactDB.js
+++ config/contactDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-
+//
 const dbContact = () => {
     mongoose.connect("mongodb+srv://siskodb:sisko007SP@cluster0.2pdvdr6.mongodb.net/todolist?retryWrites=true&w=majority")
         .then(() => {
